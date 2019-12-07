74e5f6d4e7c8e831d9523767362fd516747549ab
abhishek305 <abhishekshaji1994@gmail.com>
2019-12-08 00:20:39 +0530

    Updates

---
+++ precache-manifest.25f9f9de2934e5d9a3c57f7b46a5a122.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b87d65dc5e81335b5c8d4f89dafeb94",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "5535e4178e7abab6b0c3",
+    "url": "/todo/static/css/main.3e400a96.chunk.css"
+  },
+  {
+    "revision": "4b325b194bd89cda6686",
+    "url": "/todo/static/js/2.9ffdf530.chunk.js"
+  },
+  {
+    "revision": "5535e4178e7abab6b0c3",
+    "url": "/todo/static/js/main.0ff1d4d5.chunk.js"
+  },
+  {
+    "revision": "226abf8f7fb0bbdc244e",
+    "url": "/todo/static/js/runtime-main.f141459d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/todo/static/media/logo.25bf045c.svg"
+  }
+]);
